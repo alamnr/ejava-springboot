93b2f2afd071fe22d007c25c9a49ae0af63b28bb
alamnr <alamnr@gmail.com>
2025-02-21 09:43:41 +0600

    Added Date Test

---
+++ svc/svc-api/content-quotes-example/content-quotes-client/src/test/java/info/ejava/examples/svc/content/quotes/dto/Jackson2JaxbTest.java
@@ -0,0 +1,5 @@
+package info.ejava.examples.svc.content.quotes.dto;
+
+public class Jackson2JaxbTest {
+    
+}
---
+++ svc/svc-api/content-quotes-example/content-quotes-client/src/test/java/info/ejava/examples/svc/content/quotes/dto/JacksonJsonTest.java
@@ -0,0 +1,5 @@
+package info.ejava.examples.svc.content.quotes.dto;
+
+public class JacksonJsonTest {
+    
+}
