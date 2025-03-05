2f5b6281c8281ecf44ace686d51dbd955cc4db74
alamnr <alamnr@gmail.com>
2025-03-05 20:08:35 +0600

    added several methods for unit inbtegration test for QuoteRestTemplateNTest

--- svc/svc-api/content-quotes-example/content-quotes-svc/src/test/java/info/ejava/examples/svc/content/quotes/QuotesRestTemplateNTest.java
+++ svc/svc-api/content-quotes-example/content-quotes-svc/src/test/java/info/ejava/examples/svc/content/quotes/QuotesRestTemplateNTest.java
@@ -3,16 +3,20 @@ package info.ejava.examples.svc.content.quotes;
 import java.net.URI;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
@@ -27,6 +31,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import info.ejava.examples.svc.content.quotes.api.QuotesApi;
 import info.ejava.examples.svc.content.quotes.dto.QuoteDTO;
 import info.ejava.examples.svc.content.quotes.dto.QuoteDTOFactory;
+import info.ejava.examples.svc.content.quotes.dto.QuoteListDTO;
 import lombok.extern.slf4j.Slf4j;
 
 /*
@@ -105,5 +110,61 @@ public class QuotesRestTemplateNTest {
         URI location = UriComponentsBuilder.fromUri(baseUrl).path(QuotesApi.QUOTE_PATH).build(createdQuote.getId());
         BDDAssertions.then(response.getHeaders().getLocation()).isEqualTo(location);
         //BDDAssertions.then(response.getHeaders().getFirst(HttpHeaders.LOCATION)).isEqualTo(location);
+    }
+
+    @Test
+    public void get_quote() {
+        // given - an existing / original quote
+        QuoteDTO existingQuote = quotesFactory.make(); // hold on to the client side object
+        ResponseEntity<QuoteDTO> quoteResponse = restTemplate.postForEntity(quotesUrl, existingQuote, QuoteDTO.class);
+        log.info("Content-type: {}", quoteResponse.getHeaders().getContentType());
+        BDDAssertions.assertThat(quoteResponse.getStatusCode().is2xxSuccessful()).isTrue();
+
+        int requestId = quoteResponse.getBody().getId();
+        URI quoteUri = UriComponentsBuilder.fromUri(baseUrl).path(QuotesApi.QUOTE_PATH).build(requestId);
+        RequestEntity request  = RequestEntity.get(quoteUri).build();
+
+        // when - requesting quote by id
+        ResponseEntity<QuoteDTO> response = restTemplate.exchange(request, QuoteDTO.class);
+
+        // then 
+        BDDAssertions.then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        BDDAssertions.then(response.getBody()).isEqualTo(existingQuote.withId(requestId));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+    public void get_quotes(String mediaTypeString) {
+
+        // given - an existing / original quote
+        MediaType mediaType = MediaType.valueOf(mediaTypeString);
+        Map<Integer,QuoteDTO> existingQuotes = new HashMap<Integer, QuoteDTO>();
+        QuoteListDTO quoteList = quotesFactory.listBuilder().make(3, 3);
+        for(QuoteDTO quote : quoteList.getQuotes()) {
+            RequestEntity<QuoteDTO> request = RequestEntity.post(quotesUrl).body(quote);
+            ResponseEntity<QuoteDTO> quoteResponse = restTemplate.exchange(request, QuoteDTO.class);
+            BDDAssertions.assertThat(quoteResponse.getStatusCode().is2xxSuccessful()).isTrue();
+            QuoteDTO addedQuote = quoteResponse.getBody();
+            existingQuotes.put(addedQuote.getId(), addedQuote);
+
+        }
+
+        BDDAssertions.assertThat(existingQuotes).isNotEmpty();
+        URI quoteUri = UriComponentsBuilder.fromUri(baseUrl).path(QuotesApi.QUOTES_PATH).build().toUri();
+
+        // when - requesting quotes by id
+        ResponseEntity<QuoteListDTO> response = restTemplate.exchange(
+                RequestEntity.get(quoteUri).accept(mediaType).build(),
+                QuoteListDTO.class);
+
+        // then ... 
+        BDDAssertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        QuoteListDTO quotesPage = response.getBody();
+        BDDAssertions.then(quotesPage.getCount()).isEqualTo(existingQuotes.size());
+        for(QuoteDTO q : quotesPage.getQuotes()){
+            BDDAssertions.assertThat(existingQuotes.remove(q.getId())).isNotNull();
+        }
+        BDDAssertions.then(existingQuotes).isEmpty();
+
     }
 }
