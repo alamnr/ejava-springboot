fce2b1ebf0a427cc3703a734f1111e19611cbcdc
alamnr <alamnr@gmail.com>
2025-05-01 10:38:57 +0600

    completing Test cases for WebClient and RestTemplate

--- svc/svc-api/rpc-greeter-example/rpc-greeter-svc/src/test/java/info/ejava/examples/svc/rpc/greeter/GreeterRestTemplateNTest.java
+++ svc/svc-api/rpc-greeter-example/rpc-greeter-svc/src/test/java/info/ejava/examples/svc/rpc/greeter/GreeterRestTemplateNTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import java.net.URI;
 
 import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -15,8 +16,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.NoOpResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import info.ejava.examples.svc.rpc.GreeterApplication;
 import lombok.extern.slf4j.Slf4j;
@@ -78,6 +89,149 @@ public class GreeterRestTemplateNTest {
             assertEquals("hi", greeting);
             BDDAssertions.then(greeting).isEqualTo("hi");
             
+    }
+
+    @Test
+    void say_greeting(){
+        // given / arrange
+        URI url =  UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .path("/say/{greeting}")
+                                        .queryParam("name", "{name}")
+                                        .build("hello", "jim");
+
+        // when / act
+        ResponseEntity<String> resp = injectedRestTemplate.getForEntity(url, String.class);
+
+        // then / evaluate-assert
+        BDDAssertions.then(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        BDDAssertions.then(resp.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith("text/plain");
+        BDDAssertions.then(resp.getBody()).isEqualTo("hello , jim");
+    }
+
+    @Test
+    void no_boom(){
+        // given / arrange
+
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .path("/boom")
+                                        .queryParam("value", "whatever")
+                                        .build().toUri();
+
+        // when / act
+
+        ResponseEntity<String>  resp = injectedRestTemplate.getForEntity(url, String.class);
+
+        // then / evaluate-assert
+
+        BDDAssertions.then(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        BDDAssertions.then(resp.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith("text/plain");
+        BDDAssertions.then(resp.getBody()).isEqualTo("worked ? ");
+    }
+
+    @Test
+    void boom() {
+        // given / arrange
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .path("/boom")
+                                        .build().toUri();
+        // when / act
+        HttpClientErrorException ex = BDDAssertions.catchThrowableOfType(() -> injectedRestTemplate.getForEntity(url, String.class),
+                                                                HttpClientErrorException.class);
+        
+        // then / evaluate - assert
+        BDDAssertions.then(ex.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        BDDAssertions.then(ex.getResponseHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
+        try {
+            ErrorMessage err = new ObjectMapper().readValue(ex.getResponseBodyAsString(), ErrorMessage.class);
+        } catch (JsonMappingException e) {
+            log.error("Error - ", e);
+        } catch (JsonProcessingException e) {
+            log.error("Error - ", e);
+        }
+    }
+
+    @Test
+    void boom_return_response_not_exception(){
+        // given / arrange
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .replacePath("rpc/greeter/boom")
+                                        .build().toUri();
+
+        // configure Resttemplate to return error response , not exception
+         /*
+         RestTemplate is the only client option that allows one to bypass the exception rule and obtain an
+         error ResponseEntity from the call without exception handling. The following example shows a
+         NoOpResponseErrorHandler error handler being put in place and the caller is receiving the error
+         ResponseEntity without using exception handling.
+         */
+        RestTemplate noExceptionRestTemplate = new RestTemplate();
+        noExceptionRestTemplate.setErrorHandler(new NoOpResponseErrorHandler());
+        // when / act 
+        Assertions.assertDoesNotThrow(() -> {
+            ResponseEntity<String> response = noExceptionRestTemplate.getForEntity(url, String.class);
+        // then / evaluate - assert
+        BDDAssertions.then(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        BDDAssertions.then(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
+        ErrorMessage err = new ObjectMapper().readValue(response.getBody(), ErrorMessage.class);
+        }, "return response not exception");
+
+
+    }
+
+    @Test
+    void boy(){
+        // given / arrange
+
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .path("/boys")
+                                        .queryParam("name", "jim")
+                                         .build().toUri();
+                        
+
+        // when / act
+        ResponseEntity<String> resp = injectedRestTemplate.getForEntity(url,String.class);
+        // then / evaluate-assert
+
+        BDDAssertions.then(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        BDDAssertions.then(resp.getHeaders().getFirst(HttpHeaders.CONTENT_LOCATION)).isEqualTo(url.toString());
+        BDDAssertions.then(resp.getBody()).isEqualTo("Hello jim , how do you do ?");
+
+    }
+
+    @Test
+    void boy_blue(){
+        // given / arrange
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .path("/boys")
+                                        .queryParam("name", "blue")
+                                        .build().toUri();
+
+        // when / act
+
+        HttpClientErrorException ex = BDDAssertions.catchThrowableOfType(() -> 
+                injectedRestTemplate.getForEntity(url, String.class), HttpClientErrorException.class);
+
+        // then / evaluate - assert
+
+        BDDAssertions.then(ex.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+        BDDAssertions.then(ex.getResponseHeaders().getFirst(HttpHeaders.CONTENT_LOCATION)).isNull();
+        BDDAssertions.then(ex.getResponseBodyAsString()).isEqualTo("boy named blue");
+    }
+
+    @Test
+    void boy_blue_with_exception_handler(){
+        // given / arrange
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .path("/boys")
+                                        .queryParam("name", "blue").build().toUri();
+        // when / act
+        HttpClientErrorException ex = BDDAssertions.catchThrowableOfType(()-> injectedRestTemplate.getForEntity(url, String.class),
+                                            HttpClientErrorException.UnprocessableEntity.class);
+        // then / evaluate-assert
+        BDDAssertions.then(ex.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+        BDDAssertions.then(ex.getResponseHeaders().getFirst(HttpHeaders.CONTENT_LOCATION)).isNull();
+        BDDAssertions.then(ex.getResponseBodyAsString()).isEqualTo("boy named blue");
+
     }
 
 }
--- svc/svc-api/rpc-greeter-example/rpc-greeter-svc/src/test/java/info/ejava/examples/svc/rpc/greeter/GreeterSyncWebClientNTest.java
+++ svc/svc-api/rpc-greeter-example/rpc-greeter-svc/src/test/java/info/ejava/examples/svc/rpc/greeter/GreeterSyncWebClientNTest.java
@@ -1,6 +1,9 @@
 package info.ejava.examples.svc.rpc.greeter;
 
+import java.net.URI;
+
 import org.assertj.core.api.BDDAssertions;
+import org.assertj.core.util.DateUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -10,7 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import info.ejava.examples.svc.rpc.GreeterApplication;
 import lombok.extern.slf4j.Slf4j;
@@ -56,4 +68,114 @@ public class GreeterSyncWebClientNTest {
         // then / evaluate 
         BDDAssertions.then(response).isEqualTo("hi");
     }
+
+    @Test
+    void say_greeting(){
+        // given / arrange
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .path("/say/{greeting}")
+                                        .queryParam("name", "{name}")
+                                        .build("hello", "jim");
+        // when / act
+        ResponseEntity<String> resp = injectedWebClient.get().uri(url).retrieve().toEntity(String.class).block();
+        // then / evaluate-assert
+        BDDAssertions.then(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        BDDAssertions.then(resp.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith("text/plain");
+        BDDAssertions.then(resp.getHeaders().getFirst(HttpHeaders.CONTENT_LENGTH)).isEqualTo("11");
+        BDDAssertions.then(resp.getBody()).isEqualTo("hello , jim");
+
+    }
+
+    @Test
+    void no_boom(){
+        // given / arrange
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .replacePath("/rpc/greeter/boom")
+                                        .queryParam("value", "whatever")
+                                        .build().toUri();
+        // when / act
+        ResponseEntity<String> resp = injectedWebClient.get().uri(url).retrieve().toEntity(String.class).block();
+        // then / evaluate-assert
+        BDDAssertions.then(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        BDDAssertions.then(resp.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith("text/plain");
+        BDDAssertions.then(resp.getBody()).isEqualTo("worked ? ");
+    }
+
+    @Test
+    void boom(){
+        // given / arrange
+
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .replacePath("/rpc/greeter/boom")
+                                        .build().toUri();
+        // when / act
+        WebClientResponseException ex = BDDAssertions.catchThrowableOfType(() -> 
+                            injectedWebClient.get().uri(url).retrieve().toEntity(String.class).block(),WebClientResponseException.class);
+
+        // then / evaluate-assert
+
+        BDDAssertions.then(ex.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        BDDAssertions.then(ex.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).isEqualTo(MediaType.APPLICATION_JSON_VALUE);
+        try {
+            ErrorMessage err = new ObjectMapper().readValue(ex.getResponseBodyAsString(), ErrorMessage.class);
+            log.info("Parsed error message - {}", err);
+        } catch (JsonProcessingException e) {
+            log.error("Error message - ", e);
+        }
+    }
+
+    @Test
+    void boy() {
+        // given / arrange
+
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl)
+                                        .path("/boys").queryParam("name", "jim").build().toUri();
+
+        // when / act
+
+        ResponseEntity<String> resp = injectedWebClient.get().uri(url).retrieve().toEntity(String.class).block();
+
+
+        // then // evaluate-assert
+        BDDAssertions.then(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        BDDAssertions.then(resp.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith("text/plain");
+        BDDAssertions.then(resp.getHeaders().getFirst(HttpHeaders.CONTENT_LOCATION)).isEqualTo(url.toString());
+        BDDAssertions.then(resp.getHeaders().getFirst(HttpHeaders.CONTENT_LENGTH)).isGreaterThan("0");
+        BDDAssertions.then(resp.getBody()).isEqualTo("Hello jim , how do you do ?");
+    }
+
+    @Test
+    void boy_blue(){
+
+        // given / arrange
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl).path("/boys").queryParam("name", "blue").build().toUri();
+
+        // when / act 
+        WebClientResponseException ex = BDDAssertions.catchThrowableOfType(()->
+                                    injectedWebClient.get().uri(url).retrieve().toEntity(String.class).block(), WebClientResponseException.class);
+
+        // then / evaluate-assert
+        BDDAssertions.then(ex.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+        BDDAssertions.then(ex.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith("text/plain");
+        BDDAssertions.then(ex.getHeaders().getFirst(HttpHeaders.CONTENT_LOCATION)).isNull();
+        BDDAssertions.then(ex.getResponseBodyAsString()).isEqualTo("boy named blue");
+    }
+
+    @Test
+    void boy_blue_with_exception_handler() {
+        // given / arrange
+
+        URI url = UriComponentsBuilder.fromHttpUrl(injectedBaseUrl).path("/boys/throws").queryParam("name", "blue").build().toUri();
+        // when / act
+        WebClientResponseException ex = BDDAssertions.catchThrowableOfType(()->
+                                            injectedWebClient.get().uri(url).retrieve().toEntity(String.class).block(),
+                                            WebClientResponseException.class);
+
+        // then / evaluate-assert
+        BDDAssertions.then(ex.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+        BDDAssertions.then(ex.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith("text/plain");
+        BDDAssertions.then(ex.getHeaders().getFirst(HttpHeaders.CONTENT_LOCATION)).isNull();
+        BDDAssertions.then(ex.getResponseBodyAsString()).isEqualTo("boy named blue");
+    }
+
 }
